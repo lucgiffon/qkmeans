99d05b1774aa8e06073f51d16c34ec26d6bcd555
valentin.emiya <valentin.emiya@lif.univ-mrs.fr>
2019-04-30 10:53:28 +0200

    update display, init

--- code/pyqalm/script_hadamard_decomposition.py
+++ code/pyqalm/script_hadamard_decomposition.py
@@ -20,20 +20,22 @@ nb_keep_values = 64
 # lst_factors = [projection_operator(np.random.rand(d, d), nb_keep_values) for _ in range(nb_factors)]
 lst_factors = [np.eye(d) for _ in range(nb_factors)]
 #lst_factors = [np.random.rand(d, d) for _ in range(nb_factors)]
-lst_factors = [fac/norm(fac) for fac in lst_factors]
+# lst_factors = [fac/norm(fac) for fac in lst_factors]
 lst_factors[-1] = np.zeros((d, d))  # VE
 _lambda = 1.
 had = hadamard(d)
-H =  had / norm(had, ord='fro')
+# H =  had / norm(had, ord='fro')
+H = had
 print(H)
 
 #final_lambda, final_factors, final_X = PALM4LED(H, lst_factors, [nb_keep_values for _ in range(nb_factors)], _lambda, nb_iter)
-final_lambda, final_factors, final_X = HierarchicalPALM4MSA(H,
-                                                            lst_factors,
-                                                            nb_keep_values,
-                                                            _lambda,
-                                                            nb_iter,
-                                                            right_to_left=True)
+final_lambda, final_factors, final_X = HierarchicalPALM4MSA(
+    arr_X_target=H,
+    lst_S_init=lst_factors,
+    nb_keep_values=nb_keep_values,
+    f_lambda_init=_lambda,
+    nb_iter=nb_iter,
+    right_to_left=True)
 
 print("Lambda value: " + str(final_lambda))
 visual_evaluation_palm4msa(H, lst_factors, final_factors, final_X)
