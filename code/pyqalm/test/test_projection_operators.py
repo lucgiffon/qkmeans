137fac5a11cef26d73106c1a7165803317836b3a
Luc Giffon <luc.giffon@lis-lab.fr>
2019-04-30 09:59:43 +0200

    augment test splincol with: -test if all values remain the same after projection; - test if all kept values in line/col are greater than all removed values

--- code/pyqalm/test/test_projection_operators.py
+++ code/pyqalm/test/test_projection_operators.py
@@ -32,12 +32,31 @@ class TestProjectionOperators(unittest.TestCase):
         assert self.matrix.max() == 7
         assert len(self.matrix[self.matrix != 0]) == nb_keep_values
 
+    def assert_keep_coeffs_are_greater_in_line(self, projected, transpose=False):
+        if transpose:
+            removed_coeffs_matrix = self.matrix.T - projected
+        else:
+            removed_coeffs_matrix = self.matrix - projected
+
+        for lin_idx, _ in enumerate(projected):
+            abs_projected_coeff_lin = np.abs(projected[lin_idx])
+            abs_projected_coeff_lin_no_zero = abs_projected_coeff_lin[abs_projected_coeff_lin != 0]
+
+            abs_removed_coeff_lin = np.abs(removed_coeffs_matrix[lin_idx])
+            abs_removed_coeff_lin_no_zero = abs_removed_coeff_lin[abs_removed_coeff_lin != 0]
+            assert (abs_projected_coeff_lin_no_zero >= abs_removed_coeff_lin_no_zero).all()
+
+
     def test_splincol(self):
         nb_keep_values = 1
         projected_matrix = prox_splincol(self.matrix, nb_keep_values)
         assert all(len(lin[lin != 0]) >= 1 for lin in projected_matrix)
         assert all(len(col[col != 0]) >= 1 for col in projected_matrix.T)
-        # print(projected_matrix)
+        assert (self.matrix[projected_matrix != 0] == projected_matrix[projected_matrix != 0]).all()
+
+        self.assert_keep_coeffs_are_greater_in_line(projected_matrix)
+        self.assert_keep_coeffs_are_greater_in_line(projected_matrix.T, transpose=True)
+
 
 if __name__ == "__main__":
     unittest.main()
