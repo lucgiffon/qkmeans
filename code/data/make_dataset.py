d63bf586b5644bb263287fbae20b7fbab3ca2e12
Luc Giffon <luc.giffon@lis-lab.fr>
2019-05-22 06:10:32 +0200

    add support for plant dataset in make dataset

--- code/data/make_dataset.py
+++ code/data/make_dataset.py
@@ -7,6 +7,7 @@ import pandas
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 # from keras.datasets import mnist, fashion_mnist
+from sklearn import preprocessing
 
 # from skluc.main.utils import read_matfile, logger, download_data
 from pyqalm.utils import download_data, logger
@@ -32,11 +33,41 @@ def load_census1990():
 
     return data.values[1:] # remove the `caseId` attribute
 
+def load_plants():
+    data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/plants/plants.data"
+
+    with tempfile.TemporaryDirectory() as d_tmp:
+        logger.debug(f"Downloading file from url {data_url} to temporary directory {d_tmp}")
+        file_path = download_data(data_url, d_tmp)
+
+        with open(file_path, 'r', encoding="ISO-8859-15") as f:
+            plants = f.readlines()
+
+    set_plants_attributes = set()
+    lst_plants = []
+    for plant_line in plants:
+        plant_line_no_name = [v.strip() for v in plant_line.split(',')[1:]]
+        lst_plants.append(plant_line_no_name)
+        set_plants_attributes.update(plant_line_no_name)
+
+    arr_plants_attributes = np.array([v for v in set_plants_attributes])
+    onehot_encoder = preprocessing.OneHotEncoder(sparse=False)
+    onehot_encoder.fit(arr_plants_attributes.reshape(-1, 1))
+
+    for i, plant_line_no_name in enumerate(lst_plants):
+        plant_line_oh = np.sum(onehot_encoder.transform(np.array(plant_line_no_name).reshape(-1, 1)), axis=0)
+        lst_plants[i] = plant_line_oh
+
+    arr_lst_plants = np.array(lst_plants)
+
+    return arr_lst_plants
+
 MAP_NAME_DATASET = {
     "kddcup": load_kddcup04bio,
     # "mnist": mnist.load_data,
     # "fashion_mnist": fashion_mnist.load_data,
-    "census": load_census1990
+    "census": load_census1990,
+    "plants": load_plants
 }
 
 MAP_NAME_CLASSES_PRESENCE = {
@@ -44,6 +75,7 @@ MAP_NAME_CLASSES_PRESENCE = {
     # "mnist": True,
     # "fashion_mnist": True,
     "census": False,
+    "plants": False
 }
 
 def _download_all_data(output_dirpath):
