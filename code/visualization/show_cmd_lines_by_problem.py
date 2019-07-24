1ac2b922483ee03acd814d00460bb0e5c5856b0d
Luc Giffon <luc.giffon@lis-lab.fr>
2019-07-24 09:27:05 +0200

    vizualization for normalized nystrom error

--- code/visualization/show_cmd_lines_by_problem.py
+++ code/visualization/show_cmd_lines_by_problem.py
@@ -10,7 +10,7 @@ from pyqalm.utils import logger
 from visualization.utils import get_dct_result_files_by_root, display_cmd_lines_from_root_name_list
 
 
-src_results_dir = pathlib.Path("/home/luc/PycharmProjects/qalm_qmeans/results/2019/07/qmeans_analysis_blobs_reasonable")
+src_results_dir = pathlib.Path("/home/luc/PycharmProjects/qalm_qmeans/results/2019/07/qmeans_analysis_blobs_with_normalization")
 # src_results_dir = pathlib.Path("/home/luc/PycharmProjects/qalm_qmeans/results/2019-05/qmeans_analysis_bigdataset_3_20_ghz_cpu_mem_gt_90")
 if __name__ == "__main__":
 
