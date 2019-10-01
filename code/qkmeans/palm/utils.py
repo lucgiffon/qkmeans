c234f61a256685dfe9ca8ca1cecad0a77c5c4e20
Luc Giffon <luc.giffon@lis-lab.fr>
2019-10-01 17:22:12 +0200

    update docstring palm utils

--- code/qkmeans/palm/utils.py
+++ code/qkmeans/palm/utils.py
@@ -4,6 +4,14 @@ from qkmeans.data_structures import SparseFactors
 
 
 def compute_objective_function(arr_X_target, _f_lambda, _lst_S):
+    """
+    Compute objective function for the palm4msa algorithm: the froebenius norm of the difference between the target matrix and the reconstructed factorization.
+
+    :param arr_X_target: The target matrix
+    :param _f_lambda: The scaling factor
+    :param _lst_S: The sparse factors
+    :return:
+    """
     if isinstance(_lst_S, SparseFactors):
         reconstruct = _f_lambda * _lst_S.compute_product()
     else:
@@ -11,4 +19,11 @@ def compute_objective_function(arr_X_target, _f_lambda, _lst_S):
     return np.linalg.norm(arr_X_target - reconstruct, ord='fro') ** 2
 
 def update_scaling_factor(X, X_est):
+    """
+    Implementation of the scaling factor between the reconstructed matrix and the target matrix
+
+    :param X: Reconstructed matrix from unit norm sparse factors
+    :param X_est: Target matrix
+    :return: The new scaling factor
+    """
     return np.sum(X * X_est) / np.sum(X_est ** 2)
